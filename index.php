6d20c927656ca9f0a5f827e0ef07d64f174b3008
Helsingborgs Dagblad <noah@hd.se>
2009-05-27 22:38:25 +0200

    Properly decode encoded machine tags in set parameter when accessed via a webserver

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 	require_once 'config.flickr.php';
 	require_once 'hdFlickr.php';
-	require_once 'temp.php';
 
 
 	// UTF-8
@@ -26,23 +25,31 @@
 	}
 
 
+	// Decode encoded machine tag in argument 
+	//   1234 -> 1234
+	//   ns-foo-bar -> ns:foo=bar
 	function decodeMachineTagArgument($set_id) {
 		if(!isMachineTag($set_id))
 			return $set_id;
 
 		$temp = explode("-", $set_id, 3);
-		assert($temp == 3);
+		assert(count($temp) == 3);
 
 		return $temp[0] .":". $temp[1] ."=". $temp[2];
+	}
+
+
+	// Encode eventual machine tag in argument
+	//   ns:foo=bar -> ns-foo-bar
+	function encodeMachineTagArgument($set_id) {
+		return preg_replace(array("@:@", "@=@"), array("-", "-"), $set_id, 1);
 	}
 
 
 	// For creating a link to the page that display an image
 	function makeImageLink($id, $set_id) {
 		// If $set_id is a machine tag, encode it (replacing ':' and '=' with dashes)
-		if(preg_match("/^[a-z]/i", $set_id))
-			$set_id = preg_replace(array("@:@", "@=@"), array("-", "-"), $set_id, 1);
-
+		$set_id = encodeMachineTagArgument($set_id);
 
 		return "./?id=$id&set=$set_id";
 	}
@@ -64,14 +71,17 @@
 
 	if(isset($_GET["id"])) {
 		$img_id = $_GET["id"];
-		$set_id = $_GET["set"];
+		$set_id = decodeMachineTagArgument($_GET["set"]);
+
 		include("image.php");
 	}
 	else if(isset($_GET["set"])) {
-		$set_id = $_GET["set"];
+		$set_id = decodeMachineTagArgument($_GET["set"]);
+
 		include("photoset.php");
 	}
 	else if(isset($argv) && count($argv) > 1) {
+		// Command line support (for debugging purposes..)
 		for($argc = 1; $argc < count($argv) + 1; $argc += 2) {
 			if(!strcmp($argv[$argc], "id") && $argc < count($argv) + 2) {
 				$img_id = $argv[$argc + 1];
